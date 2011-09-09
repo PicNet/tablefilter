fd715821136dc267ce6c5dfed6f032e521cfa02f
Henrik Larsson <henrik.larsson@picnet.com.au>
2011-09-09 13:55:51 +1000

    switch back to old picnetcompiler and update release.zip since it broke events

--- update_links.bat
+++ update_links.bat
@@ -9,16 +9,16 @@ REM git clone git@github.com:PicNet/picnet_closure_repo.git
 REM git clone picnet\closure_compiler picnet_closure_compiler
 REM svn checkout https://picnet-closure-library-fork.googlecode.com/svn/trunk/
 
-set CLOSURE_LIB_SOURCE_DIR=J:\dev\Projects\Misc\picnet-closure-library-fork\closure-library
-set CLOSURE_COMPILER_SOURCE_DIR=J:\dev\Projects\Misc\picnet_closure_compiler\build\picnetcompiler.jar
+set CLOSURE_LIB_SOURCE_DIR=c:\dev\Projects\Misc\picnet-closure-library-fork\closure-library
+REM set CLOSURE_COMPILER_SOURCE_DIR=c:\dev\Projects\Misc\picnet_closure_compiler\build\picnetcompiler.jar
 
 set CLOSURE_LIB_TARGET_DIR=lib\closure-library
 set CLOSURE_COMPILER_TARGET_DIR=lib\picnetcompiler.jar
 
 rmdir %CLOSURE_LIB_TARGET_DIR%
-del %CLOSURE_COMPILER_TARGET_DIR%
+REM del %CLOSURE_COMPILER_TARGET_DIR%
 
 mklink /D %CLOSURE_LIB_TARGET_DIR% %CLOSURE_LIB_SOURCE_DIR% && ^
-mklink %CLOSURE_COMPILER_TARGET_DIR% %CLOSURE_COMPILER_SOURCE_DIR%
+REM mklink %CLOSURE_COMPILER_TARGET_DIR% %CLOSURE_COMPILER_SOURCE_DIR%
 
 PAUSE
